f9a7889f1f965b893509a421989c10f0f19f402b
jaydemike21 <jaydemike21@gmail.com>
2024-02-15 20:03:03 +0800

    Responsive Home 95%

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import './home.css';
 import profileImage from '../../src/assets/image.png'
 import { Link } from "react-router-dom";
+import { FaFacebook, FaGithub, FaInstagramSquare , FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 export const Home = () => {
     return(
+        
         <div className="container">
             <div className="container-left">
                 <h3>Hello!</h3>
@@ -12,14 +15,13 @@ export const Home = () => {
                 <p>Aspiring Full-Stack Developer and Software Developer 👨‍💻|</p>
                 <button>Download CV!</button>
 
-
                 <div className="icons">
                     <ul>
-                        <li><Link to="">Facebook</Link></li>
-                        <li><Link to="">Instagram</Link></li>
-                        <li><Link to="">Github</Link></li>
-                        <li><Link to="">Linkedin</Link></li>
-                        <li><Link to="">Leetcode</Link></li>
+                        <li><a href="#" target="_blank"><FaFacebook color="blue" size="3rem" /></a></li>
+                        <li><a href="#" target="_blank"><FaSquareXTwitter color="black" size="3rem" /></a></li>
+                        <li><a href="#" target="_blank"><FaInstagramSquare color="#FD1D1D" size="3rem" /></a></li>
+                        <li><a href="#" target="_blank"><FaLinkedin color="blue" size="3rem" /></a></li>
+                        <li><a href="#" target="_blank"><FaGithub color="black" size="3rem" /></a></li>
 
                     </ul>
                 </div>
